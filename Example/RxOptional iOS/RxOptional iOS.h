de394347cccd8b809c03d1454cddac0f48165a38
shingt <shingtshin@gmail.com>
2017-02-14 14:07:57 +0900

    Carthage support

---
+++ Example/RxOptional iOS/RxOptional iOS.h
@@ -0,0 +1,19 @@
+//
+//  RxOptional iOS.h
+//  RxOptional iOS
+//
+//  Created by GotoShinichi on 2/14/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxOptional iOS.
+FOUNDATION_EXPORT double RxOptional_iOSVersionNumber;
+
+//! Project version string for RxOptional iOS.
+FOUNDATION_EXPORT const unsigned char RxOptional_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxOptional_iOS/PublicHeader.h>
+
+
